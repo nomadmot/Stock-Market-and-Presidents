fea652c889432a1187eabc07ca2809a1f23201bb
Thomas Damon <nomadmot@mac.com>
2022-09-18 17:31:27 -0500

    added a script for the panel framework

---
+++ app-panel.py
@@ -0,0 +1,48 @@
+from unicodedata import name
+import panel as pn
+from panel.interact import interact, interactive, fixed, interact_manual
+from panel import widgets as pnw
+
+from plot_presidents import create_chart
+
+# pn.interact(create_chart, presidents=True, market=True).servable()  # the easy way
+
+# set up the selection options
+select_presidents = "Show Presidents"
+select_market = "Show Market"
+
+# create the selection checkbox
+selection = pnw.CheckBoxGroup(
+    name = "Select Plots",
+    options = [
+        select_presidents,
+        select_market
+    ],
+    value = [
+        select_presidents,
+        select_market
+    ]
+)
+# watch the selected values
+# selection.param.watch(print, "value")
+
+# helper function to unpack the selected values
+def chart_helper(selection):
+    return create_chart(
+        select_presidents in selection,
+        select_market in selection)
+
+# bind the function (there must be a better way)
+chart = pn.bind(
+    chart_helper,
+    selection)
+
+# compose a panel layout
+panel = pn.Column(
+    "<h1>Dow Jones Performance by President</h1.",
+    selection,
+    chart
+)
+
+# serve the panel
+panel.servable( title="Dow Performance")
