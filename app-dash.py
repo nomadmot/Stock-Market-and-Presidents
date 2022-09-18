09e94e624edc8c2efba7d579e54f6c5555f923d3
Thomas Damon <nomadmot@mac.com>
2022-09-18 09:26:49 -0500

    explore Dash dashboarding framework

---
+++ app-dash.py
@@ -0,0 +1,40 @@
+from dash import dcc, html, Dash
+import dash.dependencies
+from plot_presidents import create_chart
+
+app = Dash(__name__)
+
+value_presidents = "presidents" # selection value for presidents
+value_market = "market"         # selection value for markets
+
+app.layout = html.Div(children=[
+    html.Title("Dow Market Performance"),
+    html.H1("Dow Market Performance by Presidents"),
+    dcc.Checklist(              # create a checklist to select chart elements
+        id = "select_plots",
+        options = [
+            {"label": "Show Presidents", "value": value_presidents},
+            {"label": "Show Market", "value": value_market}
+        ],
+        value = [value_presidents, value_market]
+    ),
+    dcc.Graph(
+        id = "chart1"
+    )
+])
+
+# do the interaction
+@app.callback(
+    dash.dependencies.Output(component_id='chart1', component_property='figure'),
+    dash.dependencies.Input(component_id='select_plots', component_property='value')
+)
+
+def update_chart(checked_values):
+    return create_chart(                    # generate the chart based on selected vales
+        value_presidents in checked_values, # "Show Presidents" selected
+        value_market in checked_values      # "Show Market" selected
+        )
+
+# run the server
+if __name__ == '__main__':
+	app.run_server(debug=True)
